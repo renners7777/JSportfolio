270708d05d88627378e66b8170fc3fdf57ffa197
Chris Renshaw <chrisrenshaw79@gmail.com>
2022-10-22 23:00:27 +0100

    modifications made to hats index.html and index.js files

--- hello_world/hats/index.js
+++ hello_world/hats/index.js
@@ -1,4 +1,14 @@
 //Select elements
-
+const theGreeting = document.querySelector('#greeting');
+const theHats = document.querySelector('#hats');
+const theSubtotal = document.querySelector('#subtotal');
+const theShipping = document.querySelector('#shipping');
+const theGrandTotal = document.querySelector('#grandTotal');
 //create some variables that declare our content
+const greeting = 'Hello again';
+let name = 'Chris';
+let message = 'Please check your order'
+let welcome = `<h1>${greeting} ${name}, ${message} </h1>`;
+//console.log(welcome)
 //replace the content
+theGreeting.innerHTML = welcome;
